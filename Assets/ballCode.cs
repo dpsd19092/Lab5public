02fdd1eaf5c3f587693518165914af0b4b46934b
dpsd19092 <101414447+dpsd19092@users.noreply.github.com>
2023-03-14 17:04:00 +0200

    new project

---
+++ Assets/ballCode.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float acc = 10;
+    public float jump = 10;
+    public float forward = 10;
+    Rigidbody rb;
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        { rb.velocity = new Vector3(0, jump, forward); }
+
+    }
+    void FixedUpdate()
+    {
+        rb.velocity += new Vector3(Input.GetAxis("Horizontal") * acc, 0, Input.GetAxis("Vertical") * acc);
+    }
+
+    
+    
+}
